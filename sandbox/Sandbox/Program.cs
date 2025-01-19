d7952baabc79bfc5ec0dedbbc2c04ff1d38662c6
devonstanford <sta20020@byui.edu>
2025-01-18 20:04:09 -0700

    In-class coding in the sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+
+        Console.WriteLine($"I looked out the window and saw a {adjective} {noun}.");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    static string GetAdjective()
+    {
+        Console.WriteLine("Enter an adjective:");
+        string adjective = Console.ReadLine();
+        return adjective;
+    }
+
+    static string GetNoun()
+    {
+        Console.WriteLine("Enter a noun:");
+        string noun = Console.ReadLine();
+        return noun;
     }
 }
