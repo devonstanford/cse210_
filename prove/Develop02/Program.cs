c2a00aad8fbbcf7ab0fc3395f10684e1a8b074f4
devonstanford <sta20020@byui.edu>
2025-01-28 16:19:16 -0700

    Created the prompt class and worked on menus

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Prompts myPrompts = new Prompts();
+
+        bool keepRunning = true;
+
+        while (keepRunning)
+        {
+            MainMenu();
+            string mainChoice = Console.ReadLine();
+
+            switch (mainChoice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    bool runPrompts = true;
+                    while (runPrompts)
+                    {
+                        PromptMenu();
+                        string promptChoice = Console.ReadLine();
+                        switch (promptChoice)
+                        {
+                            case "1":
+                                myPrompts.New();
+                                break;
+                            case "2":
+                                myPrompts.Delete();
+                                break;
+                            case "3":
+                                myPrompts.Display();
+                                break;
+                            case "4":
+                                runPrompts = false;
+                                break;
+                            default:
+                                Console.WriteLine("Invalid choice. Please try again.");
+                                break;
+                        }
+                    }
+                    break;
+                case "3":
+                    keepRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    static void MainMenu()
+    {
+        Console.WriteLine("1. Entries");
+        Console.WriteLine("2. Prompts");
+        Console.WriteLine("3. Quit");
+    }
+
+    static void PromptMenu()
+    {
+        Console.WriteLine("1. Add a prompt");
+        Console.WriteLine("2. Delete a prompt");
+        Console.WriteLine("3. Display all prompts");
+        Console.WriteLine("4. Quit");
     }
 }
---
+++ prove/Develop02/Promps.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Dynamic;
+using System.Collections.Generic;
+
+public class Prompts
+{
+    public List<string> _promptList { get; set; }
+
+    public Prompts()
+    {
+        _promptList = new List<string>
+        {
+            "Write a story about a dragon.",
+            "Write a story about a princess.",
+            "Write a story about a knight.",
+            "Write a story about a wizard.",
+            "Write a story about a fairy.",
+            "Write a story about a pirate.",
+            "Write a story about a ghost.",
+            "Write a story about a vampire.",
+            "Write a story about a werewolf.",
+            "Write a story about a superhero."
+        };
+    }
+
+    public string Get()
+    {
+        Random rand = new Random();
+        int promptNumber = rand.Next(0, _promptList.Count);
+        return _promptList[promptNumber];
+    }
+
+    public void New()
+    {
+        Console.Write("Enter a new prompt: ");
+        string prompt = Console.ReadLine();
+        Console.Write($"The new prompt is: {prompt}. Do you want to add it? (y/n)");
+        string add = Console.ReadLine();
+        if (add == "y")
+        {
+            _promptList.Add(prompt);
+            Console.WriteLine("Prompt added.");
+        }
+        else
+        {
+            Console.WriteLine("Prompt not added.");
+        }
+    }
+
+    public void Delete()
+    {
+        Console.Write("Enter the prompt number you want to delete: ");
+        int prompt = int.Parse(Console.ReadLine());
+        Console.Write($"Thi prompt will be deleted: {_promptList[prompt-1]}. Do you want to delete it? (y/n)");
+        string delete = Console.ReadLine();
+        if (delete == "y")
+        {
+            _promptList.Remove(_promptList[prompt-1]);
+            Console.WriteLine("Prompt deleted.");
+        }
+        if (delete == "n")
+        {
+            Console.WriteLine("Prompt not deleted.");
+        }
+    }
+
+    public void Display()
+    {
+        for (int i = 0; i < _promptList.Count; i++)
+        {
+            Console.WriteLine($"{i+1}: {_promptList[i]}");
+        }
+    }
+}
