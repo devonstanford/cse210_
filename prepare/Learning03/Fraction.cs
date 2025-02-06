750317f2c5002db587961d25224983f152511e94
devonstanford <sta20020@byui.edu>
2025-02-05 22:27:47 -0700

    Learning 03 is complete and functional

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,64 @@
+class Fraction
+{
+    // Atributes
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            _bottom = 1;
+        }
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
